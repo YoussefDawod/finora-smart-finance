9717565a1738edc0b8d828d945acc2369b90827c
Youssef Dawod <youssef.dawod@dci-student.org>
2026-02-04 06:08:29 +0100

    refactor(i18n): split translations and remove plan doc

---
+++ finora-smart-finance-frontend/src/i18n/translations/ar.js
@@ -0,0 +1,8 @@
+import translation from '../../../public/locales/ar/translation.json';
+
+const ar = {
+  translation,
+};
+
+export default ar;
+
---
+++ finora-smart-finance-frontend/src/i18n/translations/en.js
@@ -0,0 +1,8 @@
+import translation from '../../../public/locales/en/translation.json';
+
+const en = {
+  translation,
+};
+
+export default en;
+
